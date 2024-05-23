4fcf16ebe42cd37f66e48332f34a5ca9ca8043d2
Daiana de Paula <daianaadepaula1@gmail.com>
2024-05-23 10:34:05 -0300

    feat: setup project

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+import { fontFamily } from "tailwindcss/defaultTheme";
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+			fontFamily: {
+        sans: ["var(--font-sora)", ...fontFamily.sans],
+      },
+		},
+  },
+  plugins: [],
+}
